cb3b767edf863aabc6f806640356bda9a9e1153f
Adriana Sanchez <asanchez@adri-mac.local>
2022-06-16 23:49:51 -0400

    chane route

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 //import PaintProducts from './Components/PaintProducts';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('App'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
